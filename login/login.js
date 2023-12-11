11125399ec21e6a984c7454193c24d88fe16b86c
Andra <111245270+Andra-sun@users.noreply.github.com>
2023-12-11 09:19:54 -0300

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -1,3 +1,4 @@
+// trocar de pagina
 document.addEventListener("DOMContentLoaded", function () {
     const botaoIrProLogin = document.querySelector("#botao-ir-pro-login");
     const botaoIrProRegistro = document.querySelector("#botao-ir-pro-registro");
@@ -14,3 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
         login.style.display = "none";
     });
 });
+
+// mostrar senha
+function toggleSenha(formulario) {
+    var senhaInput = document.getElementById(formulario + "-senha");
+    var olhoImg = document.getElementById(formulario + "-mostrar-senha");
+
+    if (senhaInput.type === "password") {
+        senhaInput.type = "text";
+        olhoImg.src = "https://cdn-icons-png.flaticon.com/128/2874/2874802.png";
+    } else {
+        senhaInput.type = "password";
+        olhoImg.src =
+            "https://cdn-icons-png.flaticon.com/128/10812/10812267.png";
+    }
+};
