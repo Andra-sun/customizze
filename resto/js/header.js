7f83c4c9ea42de90627d1f66923f6172caf29cb6
Andra-sun <camileguimaraes463@gmail.com>
2023-12-11 10:43:27 -0300

    atualização no header

--- resto/js/header.js
+++ resto/js/header.js
@@ -1,10 +1,20 @@
 const abrir = document.querySelector(".click-for-contato");
-const caixa = document.querySelector('.contato');
+const caixa = document.querySelector(".contato");
 
-abrir.addEventListener('click', function () {
+abrir.addEventListener("click", function () {
     if (caixa.style.display === "none" || caixa.style.display === "") {
         caixa.style.display = "flex";
     } else {
         caixa.style.display = "none";
     }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const profileButton = document.querySelector(".perfio");
+    const profileBar = document.querySelector(".profile-bar");
+
+    profileButton.addEventListener("click", function () {
+        // Toggle entre display: none e display: flex
+        profileBar.style.display = (profileBar.style.display === "none" || profileBar.style.display === "") ? "flex" : "none";
+    });
 });
