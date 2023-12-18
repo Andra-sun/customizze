1640bb042b80833d55cf6fc830105409e4c8d1dc
Andra-sun <camileguimaraes463@gmail.com>
2023-12-18 16:17:43 -0300

    falta resolver o bug do carrossel

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,18 +1,29 @@
-const carrosselContainer = document.querySelector('.carrossel');
+function isMobile() {
+    return window.innerWidth <= 768;
+}
+
+const carrosselContainer = document.querySelector(".carrossel");
 
 function nextSlide() {
-    carrosselContainer.appendChild(carrosselContainer.firstElementChild.cloneNode(true));
+    carrosselContainer.appendChild(
+        carrosselContainer.firstElementChild.cloneNode(true)
+    );
     carrosselContainer.removeChild(carrosselContainer.firstElementChild);
 
-    const carrosselItems = document.querySelectorAll('.carrossel1, .carrossel11, .carrossel111');
+    const carrosselItems = document.querySelectorAll(
+        ".carrossel1, .carrossel11, .carrossel111"
+    );
     carrosselItems.forEach((item, index) => {
         if (index === 0) {
-            item.style.left = '0';
-            item.style.display = 'flex';
+            item.style.left = "0";
+            item.style.display = "flex";
         } else {
             item.style.left = `${index * 100}%`;
-            item.style.display = 'none';
+            item.style.display = "none";
         }
     });
 }
-setInterval(nextSlide, 2300);
+
+if (isMobile()) {
+    setInterval(nextSlide, 2300);
+}   
