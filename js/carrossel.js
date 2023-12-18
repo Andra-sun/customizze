a3b2d4fcafff023c390dd73272cfc45945041080
Andra-sun <camileguimaraes463@gmail.com>
2023-12-18 15:59:56 -0300

    carrossel pra celular feito

---
+++ js/carrossel.js
@@ -0,0 +1,18 @@
+const carrosselContainer = document.querySelector('.carrossel');
+
+function nextSlide() {
+    carrosselContainer.appendChild(carrosselContainer.firstElementChild.cloneNode(true));
+    carrosselContainer.removeChild(carrosselContainer.firstElementChild);
+
+    const carrosselItems = document.querySelectorAll('.carrossel1, .carrossel11, .carrossel111');
+    carrosselItems.forEach((item, index) => {
+        if (index === 0) {
+            item.style.left = '0';
+            item.style.display = 'flex';
+        } else {
+            item.style.left = `${index * 100}%`;
+            item.style.display = 'none';
+        }
+    });
+}
+setInterval(nextSlide, 2300);
